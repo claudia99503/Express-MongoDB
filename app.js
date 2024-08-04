a4be6b26a133ec28fec8208d89683f2ce6ecee08
minseokim <claudia503@naver.com>
2024-08-04 15:09:04 +0900

    feat: nodemon 추가

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/hello', (req, res) => {
-    res.send("hello Express!");
+    res.send("bye Express!");
 });
 
 app.listen(3000, () => console.log('Sever Started'));

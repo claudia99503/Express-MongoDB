0e09ad68afba398b4b33d572f06b3b8baef785d2
minseokim <claudia503@naver.com>
2024-08-04 15:41:37 +0900

    feat: 쿼리스트링 처리하기

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ import tasks from './data/mock.js';
 const app = express();
 
 app.get('/tasks', (req, res) => {
-    res.send(tasks);
+    /**
+     * 쿼리 파라미터
+     * - sort: 'oldest'인 경우 오래된 태스크 기준, 나머지 경우 새로운 태스크 기준
+     * - count: 태스크 개수
+     */
+    const sort = req.query.sort;
+    const count = Number(req.query.count);
+
+    const compareFn =
+        sort === 'oldest'
+            ? (a, b) => a.createdAt - b.createdAt
+            : (a, b) => b.createdAt - a.createdAt;
+    
+    let newTasks = tasks.sort(compareFn);
+
+    if (count) {
+        newTasks = newTasks.slice(0, count);
+    }
+
+    res.send(newTasks);
 });
 
 app.listen(3000, () => console.log('Sever Started'));
